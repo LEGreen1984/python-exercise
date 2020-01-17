d59a2460f55352a44102e997d74f3c06c1a41e9e
System Administrator <root@Aeriths-MacBook-Air.local>
2020-01-17 13:46:33 +0000

    first commit

---
+++ firstpython.py
@@ -0,0 +1,11 @@
+import datetime
+print("hello world!")
+age = input("what is your age?")
+century = int(2019 - age) + 100
+print("you will be 100 years old in the year {0}").format(century)
+
+number = input("enter a number")
+if int(number) % 2 == 0:
+    print("you have an even number")
+else:
+    print("we have an odd number")
